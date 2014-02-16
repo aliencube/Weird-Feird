a366af7f00ab825e4bb1753648bc49f610297cc8
Justin Yoo <justin.yoo@aliencube.com>
2014-02-16 12:19:27 +1100

    Added enum project.

---
+++ SourceCodes/WeirdFeird.Enums/FeedType.cs
@@ -0,0 +1,23 @@
+﻿namespace Aliencube.WeirdFeird.Enums
+{
+    /// <summary>
+    /// This determines the feed type.
+    /// </summary>
+    public enum FeedType
+    {
+        /// <summary>
+        /// Unknown feed type.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// ATOM feed type.
+        /// </summary>
+        Atom = 1,
+
+        /// <summary>
+        /// RSS feed type.
+        /// </summary>
+        Rss = 2,
+    }
+}
---
+++ SourceCodes/WeirdFeird.Enums/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.WeirdFeird.Enums")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Aliencube.WeirdFeird.Enums")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5fd7f2b6-3e13-4749-b173-5584fd57d9fb")]
