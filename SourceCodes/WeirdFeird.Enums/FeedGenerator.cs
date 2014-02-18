7e0f5089fe0ccee842981b73e0109e0633330b22
Justin Yoo <justin.yoo@aliencube.com>
2014-02-18 22:28:18 +1100

    Added FeedGenerator enum.

---
+++ SourceCodes/WeirdFeird.Enums/FeedGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Aliencube.WeirdFeird.Enums
+{
+    /// <summary>
+    /// This determines the feed generator.
+    /// </summary>
+    public enum FeedGenerator
+    {
+        /// <summary>
+        /// Unknown feed generator.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Wordpress.
+        /// </summary>
+        Wordpress = 1,
+    }
+}
